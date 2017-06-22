9b864ff8d92c66da3cddf5c3fdb41ded83b6429a
wahula4 <mawahula4@gmail.com>
2017-06-21 20:31:26 -0600

    updated javascript to include api call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,17 @@
+ // $("#search-button").on("click", function() {
 
+  	// var city = $("#city-input").val().trim();
+  	var queryURL = "http://beermapping.com/webservice/loccity/1e07e953394846ca559ab1d498fb5b41/" + "denver,co" + "&s=json";
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+        .done(function(response) {
+        	console.log(response);
+        	for (var i = 0; i < response.length; i++) {
+        		$("#breweries").append(response[i].name)
+        	}
+        	
+        });
+
+// });
